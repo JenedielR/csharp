6ebed35ab2280a66f02d98eae48b7ddac3045f17
JenedielR <jrodriguez2501@student.stcc.edu>
2026-02-24 09:53:34 -0500

    Create Exercise_2-7.cs

---
+++ Exercise_2-7.cs
@@ -0,0 +1,14 @@
+// HoursAndMinutes.cs
+
+using System;
+using static System.Console;
+using System.Globalization;
+
+class HoursAndMinutes
+{
+  static void Main()
+  {
+    int minutes = 197;
+    
+  }
+}
