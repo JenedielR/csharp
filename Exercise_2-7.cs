2b071f224bf302e50a1c65551caa0fadb2e75867
JenedielR <jrodriguez2501@student.stcc.edu>
2026-02-24 10:01:47 -0500

    Update Exercise_2-7.cs

--- Exercise_2-7.cs
+++ Exercise_2-7.cs
@@ -8,7 +8,12 @@ class HoursAndMinutes
 {
   static void Main()
   {
+    // INPUT (gather the info we're gonna use)
     int minutes = 197;
-    
+    int hours = minutes / 60;
+    // LOGIC (calculate results)
+    int minsLeft = minutes % 60;
+    // OUTPUT (display results to user)
+    WriteLine(minutes + " minutes is " + hours " hours and " + minsLeft + " minutes.");
   }
 }
